57505ca1a2090395fed12d6cb56926a6f3250cf8
Mindaugas Žvirblis <mz@9g.lt>
2020-07-07 18:36:17 +0300

    debounce

--- src/Button.cpp
+++ src/Button.cpp
@@ -11,6 +11,18 @@ Button::Button(int pin, int state) {
     }
 }
 
+
+Button::Button(int pin, int state, unsigned long d) {
+    _pin = pin;
+    _state = state;
+    delay = d;
+    if(_state == HIGH) {
+        pinMode(_pin, INPUT); // should be INPUT_PULLDOWN in feature
+    } else {
+        pinMode(_pin, INPUT_PULLUP);
+    }
+}
+
 bool Button::down() {
     return Button::check();
 }
@@ -23,5 +35,23 @@ bool Button::check() {
             return true;
         } 
     }
+    return false;
+}
+
+bool Button::debounce() {
+    raw = digitalRead(_pin);
+    if(raw != lastState) {
+        lasttime = millis();
+    }
+
+    if ((millis() - lasttime) >= delay) {
+        if(raw != currentState) {
+            currentState = raw;
+            if(raw == _state) {
+                return true;
+            }
+        } 
+    }
+    lastState = raw;
     return false;
 }
--- src/Button.h
+++ src/Button.h
@@ -5,8 +5,13 @@ class Button {
         int raw;
         int _state;
         int lastState;
+        int currentState;
+        unsigned long lasttime;
+        unsigned long delay = 50;
     public:
     Button(int,int);
+    Button(int,int, unsigned long);
     bool check();
     bool down();
+    bool debounce();
 };
