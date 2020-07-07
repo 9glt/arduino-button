91ace7bba586f8a4805e9306ea1f4009355c40cb
Mindaugas Žvirblis <mz@9g.lt>
2020-07-07 18:39:44 +0300

    Example

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -1,15 +1,17 @@
 #include "Button.h"
 
-Button button1(7, LOW); // LOW to use INPUT_PULLUP, HIGH - FLoating(for now)
+Button button1(7, LOW); // LOW for input pullup HIGH for INPUT
+Button button2(8, LOW, 1000); // 1000 - debaunce delay
 
 void setup() {
-// put your setup code here, to run once:
     Serial.begin(9600);
 }
 
 void loop() {
-// put your main code here, to run repeatedly:
     if(button1.down()) {
         Serial.println("CLICK!");
+    }
+    if(button2.debounce()) {
+        Serial.println("debounce click");
     }
 }
